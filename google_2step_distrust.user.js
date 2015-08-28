0076c66c1185c158f46c4435463949e0d58f8136
Jessica Creighton <jess@femtobit.org>
2015-08-28 04:53:22 -0400

    Added Author and changed the download source

--- google_2step_distrust.user.js
+++ google_2step_distrust.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name           Google 2-Step Distrust
 // @namespace      femtobit.org
+// @author         Jessica Creighton
 // @include        https://accounts.google.com/SecondFactor*
 // @include        https://accounts.google.com/signin/challenge*
-// @downloadURL    https://cdn.rawgit.com/nekojess/gm_scripts/master/google_2step_distrust.user.js
+// @downloadURL    https://raw.githubusercontent.com/nekojess/gm_scripts/master/google_2step_distrust.user.js
 // @description    Stop Google 2-Step login defaulting to trusting the computer
-// @version        0.2.1
+// @version        0.2.3
 // ==/UserScript==
 (
     function() {
