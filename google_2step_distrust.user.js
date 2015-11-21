d4a9452867bf911cfddecce2582c824111a9ac24
Jessica Creighton <jess@femtobit.org>
2015-11-21 15:24:11 -0500

    Update Google 2-Step Distrust

--- google_2step_distrust.user.js
+++ google_2step_distrust.user.js
@@ -6,19 +6,19 @@
 // @include        https://accounts.google.com/signin/challenge*
 // @downloadURL    https://raw.githubusercontent.com/nekojess/gm_scripts/master/google_2step_distrust.user.js
 // @description    Stop Google 2-Step login defaulting to trusting the computer
-// @version        0.2.3
+// @version        0.3.5
 // ==/UserScript==
-(
-    function() {
-        window.addEventListener(
-            'load',
-            function() {
-                var elm = document.getElementById('PersistentCookie');
-                if (elm) { elm.checked = false; }
-                elm = document.getElementById('trustDevice');
-                if (elm) { elm.checked = false; }
-            },
-            true
-        );
-    }
-)();
+(function () {
+	disableTrust = function() {
+		var elm = document.getElementById('trustDevice');
+		if (!elm) { 
+			elm = document.getElementById('PersistentCookie');
+		}
+		if (elm) {
+			elm.checked = false;
+		} else {
+			setTimeout(disableTrust, 50);
+		}
+	}
+	disableTrust();
+})();
